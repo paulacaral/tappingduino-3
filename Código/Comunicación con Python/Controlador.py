7f373c2a508d5350ae57cd74f34459758f5f81a4
Paula <paulacaral@hotmail.com>
2019-10-29 16:51:25 -0300

    Guarda datos en npz
    Controlador que guarda datos de cada trial en un npz. Guarda ademas otro archivo npz que tiene la variable trials que dice cuales fueron validos y cuales no.

--- Código/Comunicación con Python/Controlador.py
+++ Código/Comunicación con Python/Controlador.py
@@ -31,23 +31,35 @@ trial = 0
 bloque = 1;
 raw_input("Press Enter to start trial")
 timestr = time.strftime("%Y_%m_%d-%H.%M.%S")
-filename_valid_trials = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-trials.txt" 
-f_trial = open(filename_valid_trials,"a+")
-           
+#==============================================================================
+# 
+# filename_valid_trials = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-trials.txt" 
+# f_trial = open(filename_valid_trials,"a+")
+#            
+#==============================================================================
+valid_trial = []
+filename_trial = name+"-"+"bloque"+str(bloque)+"-trials"
+
 while (trial < cant_trials):
-    # Genero todos los archivos: crudo, stim, resp y asynch
-    filename_raw = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-"+"trial"+str(trial)+"-raw.txt"
-    filename_stim = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-"+"trial"+str(trial)+"-stim.txt"
-    filename_resp = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-"+"trial"+str(trial)+"-resp.txt"
-    filename_asynch = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-"+"trial"+str(trial)+"-asynch.txt"
-    
-    f_raw = open(filename_raw,"w+")
-    f_stim = open(filename_stim,"w+")
-    f_resp = open(filename_resp,"w+")
-    f_asynch = open(filename_asynch,"w+")
+#==============================================================================
+#     # Genero todos los archivos: crudo, stim, resp y asynch
+#     filename_raw = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-"+"trial"+str(trial)+"-raw.txt"
+#     filename_stim = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-"+"trial"+str(trial)+"-stim.txt"
+#     filename_resp = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-"+"trial"+str(trial)+"-resp.txt"
+#     filename_asynch = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-"+"trial"+str(trial)+"-asynch.txt"
+#     
+#     f_raw = open(filename_raw,"w+")
+#     f_stim = open(filename_stim,"w+")
+#     f_resp = open(filename_resp,"w+")
+#     f_asynch = open(filename_asynch,"w+")
+# 
+#==============================================================================
 
-
+    filename_data = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-"+"trial"+str(trial)
         
+    filename_data_load = name+"-"+timestr+"-"+"bloque"+str(bloque)+"-"+"trial"
+
+
     espera = random.randrange(10,20,1)/10.0
     time.sleep(espera)
     
@@ -60,7 +72,7 @@ while (trial < cant_trials):
     aux = arduino.readline()
     while (aux[0]!='E'):
         data.append(aux);
-        f_raw.write(aux); # guarda los datos crudos
+#        f_raw.write(aux); # guarda los datos crudos
         aux = arduino.readline();
         
     # Separa los datos en tipo, numero y tiempo
@@ -83,23 +95,25 @@ while (trial < cant_trials):
         if e_type[events]=='S':
             stim_number.append(e_number[events])
             stim_time.append(e_time[events])
-            f_stim.write(str(e_time[events])+"\n")   
+#            f_stim.write(str(e_time[events])+"\n")   
 
             
         if e_type[events]=='F':
             resp_number.append(e_number[events])
             resp_time.append(e_time[events])
-            f_resp.write(str(e_time[events])+"\n")
+#            f_resp.write(str(e_time[events])+"\n")
 
     long_stim = len(stim_time)
     long_resp = len(resp_time)
 
 
-               
-    f_raw.close()
-    f_stim.close()        
-    f_resp.close()
-
+#==============================================================================
+#                
+#     f_raw.close()
+#     f_stim.close()        
+#     f_resp.close()
+# 
+#==============================================================================
 
     # Calcula asincronias 
     
@@ -129,17 +143,19 @@ while (trial < cant_trials):
     
 # Se fija si el trial es valido. Si lo es, entonces guarda los datos, calcula las asincronias y devuelve los graficos. Si no, no. 
    
+    asynchrony = []
+
     if long_stim_pares == long_resp_pares:
                 
-        f_trial.write(str(trial)+"\t 1 \n")
+        valid_trial.append(1)
+#        f_trial.write(str(trial)+"\t 1 \n")
         # Calcula y guarda asincronias
-        asynchrony = []
         
         for k in range(long_stim_pares):
             asynchrony.append(stim_pares[k]-resp_pares[k])
-            f_asynch.write(str(stim_pares[k]-resp_pares[k])+"\n")
+#            f_asynch.write(str(stim_pares[k]-resp_pares[k])+"\n")
     
-        f_asynch.close()
+#        f_asynch.close()
     #==============================================================================
     # Grafica los resultados
 
@@ -199,14 +215,29 @@ while (trial < cant_trials):
         trial = trial + 1;
     
     else:
-        f_trial.write(str(trial)+"\t 0 \n")
+        valid_trial.append(0)
+#        f_trial.write(str(trial)+"\t 0 \n")
 
-        f_asynch.close()
+#        f_asynch.close()
         print("Hay que repetir el trial")
         raw_input("Press Enter to start trial")
         trial = trial + 1;
         cant_trials = cant_trials+1;
-        
-f_trial.close()
     
+    np.savez_compressed(filename_data, crudo=data, stim=stim_time, resp=resp_time, asynch=asynchrony)
+        
+#f_trial.close()
+    
+np.savez_compressed(filename_trial,trials=valid_trial)
 
+#%%
+
+npztrial = np.load(filename_trial+".npz")
+npztrial["trials"]
+
+trial1 = np.load(filename_data_load+"1.npz")
+
+trial1["crudo"]
+trial1["stim"]
+trial1["resp"]
+trial1["asynch"]
