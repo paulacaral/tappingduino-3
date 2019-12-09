f463adfe810804eac83bf7ee828fe9bcc8d68ab5
paulacaral <48900210+paulacaral@users.noreply.github.com>
2019-12-09 13:36:44 -0300

    Actualizacion controlador arduino
    Cambio de F por R en type of event (tambien en python)

--- Código/Comunicación con Python/ControladorSoundWhile/ControladorSoundWhile.ino
+++ Código/Comunicación con Python/ControladorSoundWhile/ControladorSoundWhile.ino
@@ -5,11 +5,11 @@
 //Manda estímulo al pin 11 y al pin 12 (timer 1)
 //y feedback al pin 5 y al pin 2 (timer 3)
 
-#define stimPinR 11
-#define stimPinL 12
-#define fdbkPinR 5
-#define fdbkPinL 2
-#define noisePinR 10
+#define stimPinR 12
+#define stimPinL 11
+#define fdbkPinR 2
+#define fdbkPinL 5
+#define noisePinR 9
 #define noisePinL 9
 #define pinVG A11
 
@@ -17,10 +17,10 @@
 #define LFSR_MASK  ((unsigned long)( 1UL<<31 | 1UL <<15 | 1UL <<2 | 1UL <<1  ))
 
 //http://sphinx.mythic-beasts.com/~markt/ATmega-timers.html
-boolean stimRight=true;  //pin11 - timer 1, channel A: right
-boolean stimLeft=true;  //pin12 - timer 1, channel B: left
-boolean fdbkRight=true; //pin5 - timer 3, channel A: right
-boolean fdbkLeft=true;  //pin2 - timer 3, channel B: left
+boolean stimRight=true;  //pin12 - timer 1, channel B: right
+boolean stimLeft=true;  //pin11 - timer 1, channel A: left
+boolean fdbkRight=true; //pin2 - timer 3, channel B: right
+boolean fdbkLeft=true;  //pin5 - timer 3, channel A: left
 boolean noise = false;
 
 unsigned long int t=0;
@@ -126,9 +126,9 @@ ISR(TIMER1_OVF_vect) {
     aux1 = pgm_read_byte(&sineTable[index1]);
   // Read oscillator value for next interrupt
     
-    if (stimRight==true)  OCR1A = aux1;
+    if (stimLeft==true)  OCR1A = aux1;
 
-    if (stimLeft==true)   OCR1B = aux1;
+    if (stimRight==true)   OCR1B = aux1;
   
   }  
  }
@@ -158,9 +158,9 @@ ISR(TIMER3_OVF_vect) {
       aux3 = pgm_read_byte(&sineTable[index3]);
     // Read oscillator value for next interrupt
    
-      if (fdbkRight==true)  OCR3A = aux3;
+      if (fdbkLeft==true)  OCR3A = aux3;
   
-      if (fdbkLeft==true)   OCR3B = aux3;
+      if (fdbkRight==true)   OCR3B = aux3;
     
     }  
    }
@@ -188,7 +188,7 @@ void initTimers(void){
   TCCR1A= _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11);
 
   // Start timer without prescaler
-   TCCR1B = _BV(WGM12) | _BV(CS10);
+  TCCR1B = _BV(WGM12) | _BV(CS10);
   // Enable overflow interrupt for OCR1A
   TIMSK1 = _BV(TOIE1);
 
@@ -445,7 +445,7 @@ void save_data(char ename, unsigned int enumber, unsigned long etime){
           stim_number++;
           break;
 
-        case 'F':
+        case 'R':
           fdbk_number++;
           break;
       }    
@@ -511,7 +511,7 @@ void loop() {
         SoundSwitch('f', fdbkFreq, FL, FR);
         prevFdbk_t=t;
         fdbk_flag=true;
-        save_data('F', fdbk_number, t);
+        save_data('R', fdbk_number, t);
       }
     }
 
